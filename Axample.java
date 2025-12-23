0ceba0bcafbc7b1033d252fa3d8514d43e24df1a
Samiullah Alam <asamiullah084@gmail.com>
2025-12-23 10:14:06 +0530

    initial add

---
+++ Axample.java
@@ -0,0 +1,12 @@
+public class Axample {
+
+    public static void main(String[] args) {
+        int[] marks = new int[5];
+        marks[0] = 97;
+        marks[1] = 98;
+        marks[2] = 100;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
